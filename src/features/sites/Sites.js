bcceb6ca14ad6d27e5f283a7c7ef8f6276a03640
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-01-02 20:07:33 +0100

    Adds sites page header

--- src/features/sites/Sites.js
+++ src/features/sites/Sites.js
@@ -1,55 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 import { useQuery, useMutation } from "@apollo/react-hooks";
-import { Table, HeadCell, BodyCell, Button } from "malcomponents";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalButton,
+  HeaderNavigation,
+  NavigationList,
+  NavigationItem,
+  ButtonGroup,
+  ThemeProvider,
+  DarkTheme
+} from "malcomponents";
 
 import queries from "../../api/queries";
 import mutations from "../../api/mutations";
 
 import AddSiteForm from "./AddSiteForm";
 
-const Container = styled.div`
-  margin: 1rem;
-`;
-
-const TableWrapper = styled.div``;
-
 function Sites() {
+  const [selected, setSelected] = React.useState([0]);
+  const [isOpen, setIsOpen] = React.useState(false);
   const { loading, error, data } = useQuery(queries.FIND_MY_SITES_QUERY);
   const [destroySite, destroySiteResult] = useMutation(mutations.DESTROY_SITE);
 
   return (
-    <Container>
-      <TableWrapper>
-        <Table gridTemplateColumns="2fr 1fr auto">
-          <HeadCell>Site name</HeadCell>
-          <HeadCell>Site ID</HeadCell>
-          <HeadCell>Delete</HeadCell>
-          {data &&
-            data.findMySites &&
-            data.findMySites.sites &&
-            data.findMySites.sites.map(site => {
-              return (
-                <React.Fragment key={site.id}>
-                  <BodyCell>{site.name}</BodyCell>
-                  <BodyCell>{site.siteId}</BodyCell>
-                  <BodyCell>
-                    <Button
-                      size="compact"
-                      onClick={() =>
-                        destroySite({ variables: { siteId: site.id } })
-                      }
-                    >
-                      Delete
-                    </Button>
-                  </BodyCell>
-                </React.Fragment>
-              );
-            })}
-        </Table>
-      </TableWrapper>
-      <AddSiteForm />
-    </Container>
+    <div>
+      <ThemeProvider theme={DarkTheme}>
+        <HeaderNavigation>
+          <NavigationList />
+          <NavigationList align="center">
+            <NavigationItem>
+              <ButtonGroup
+                selected={selected}
+                onClick={(event, index) => {
+                  setSelected([index]);
+                }}
+              >
+                <Button>Private</Button>
+                <Button>Public</Button>
+              </ButtonGroup>
+            </NavigationItem>
+          </NavigationList>
+          <NavigationList align="right">
+            <NavigationItem>
+              <Button onClick={() => setIsOpen(true)}>New site</Button>
+            </NavigationItem>
+          </NavigationList>
+        </HeaderNavigation>
+      </ThemeProvider>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <ModalHeader>Add a new site</ModalHeader>
+        <ModalBody>
+          <AddSiteForm onSuccess={() => setIsOpen(false)} />
+        </ModalBody>
+      </Modal>
+    </div>
   );
 }
 
