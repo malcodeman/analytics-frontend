52a0cf90a71cd170fec94cd0e44fdafd2cf6ee0e
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-02-02 16:45:11 +0100

    Adds verify site code success notification

---
+++ src/components/notification/Notification.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import { KIND } from "./constants";
+
+function getBackgroundColor(props) {
+  const { kind } = props;
+
+  switch (kind) {
+    default:
+    case KIND.info:
+      return props.theme.colors.notificationInfoBackground;
+    case KIND.positive:
+      return props.theme.colors.notificationPositiveBackground;
+    case KIND.warning:
+      return props.theme.colors.notificationWarningBackground;
+    case KIND.negative:
+      return props.theme.colors.notificationNegativeBackground;
+  }
+}
+
+function getColor(props) {
+  const { kind } = props;
+
+  switch (kind) {
+    default:
+    case KIND.info:
+      return props.theme.colors.notificationInfoText;
+    case KIND.positive:
+      return props.theme.colors.notificationPositiveText;
+    case KIND.warning:
+      return props.theme.colors.notificationWarningText;
+    case KIND.negative:
+      return props.theme.colors.notificationNegativeText;
+  }
+}
+
+const StyledNofitication = styled.div`
+  padding: 1rem;
+  background-color: ${getBackgroundColor};
+  color: ${getColor};
+  ${props => props.theme.typography.font250}
+`;
+
+function Notification(props) {
+  const { kind, children } = props;
+
+  return <StyledNofitication kind={kind}>{children}</StyledNofitication>;
+}
+
+Notification.propTypes = {
+  kind: PropTypes.oneOf([
+    KIND.info,
+    KIND.positive,
+    KIND.warning,
+    KIND.negative
+  ]),
+  children: PropTypes.element
+};
+
+Notification.defaultProps = {
+  kind: KIND.info
+};
+
+export default Notification;
--- src/features/sites/VerifySiteCode.js
+++ src/features/sites/VerifySiteCode.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { useQuery } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
 
 import {
   Modal,
@@ -12,31 +15,67 @@ import { FormControl } from "../../components/form-control";
 import { Input } from "../../components/input";
 import { Textarea } from "../../components/textarea";
 import constants from "../../constants";
+import queries from "../../api/queries";
+import { Notification, KIND } from "../../components/notification";
+
+const StyledTextarea = styled(Textarea)`
+  height: 128px;
+`;
 
 function VerifySiteCode(props) {
   const { isOpen, onClose, name, siteId } = props;
   const code = `<script defer src="${constants.ANALYTICS_SCRIPT_SOURCE}" siteId="${siteId}" graphqlUri="${constants.GRAPHQL_URI}"></script>`;
+  const { loading, error, data, refetch } = useQuery(
+    queries.FIND_DASHBOARD_QUERY,
+    {
+      skip: !siteId,
+      variables: { siteId }
+    }
+  );
+  const [isVerified, setIsVerified] = React.useState(false);
+  const history = useHistory();
+
+  React.useEffect(() => {
+    if (data && data.findDashboard && data.findDashboard.pageViews) {
+      setIsVerified(true);
+    }
+  }, [data]);
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalHeader>{name} embed code</ModalHeader>
+      <ModalHeader>{isVerified ? null : `${name} embed code`}</ModalHeader>
       <ModalBody>
-        To start securely tracking data, please add this code to the bottom of
-        your website.
-        <FormControl label="Site ID">
-          <Input value={siteId} />
-        </FormControl>
-        <FormControl label="HTML embed code">
-          <Textarea value={code} />
-        </FormControl>
+        {isVerified ? (
+          <Notification kind={KIND.positive}>
+            <span>The tracker is installed</span>
+          </Notification>
+        ) : (
+          <>
+            {error && error.message}
+            To start securely tracking data, please add this code to the head of
+            your website.
+            <FormControl label="Site ID">
+              <Input value={siteId} />
+            </FormControl>
+            <FormControl label="HTML embed code">
+              <StyledTextarea value={code} />
+            </FormControl>
+          </>
+        )}
       </ModalBody>
       <ModalFooter>
         <ModalButton onClick={onClose}>
           <span>Close</span>
         </ModalButton>
-        <ModalButton>
-          <span>Verify site code</span>
-        </ModalButton>
+        {isVerified ? (
+          <ModalButton onClick={() => history.push("/")}>
+            <span>Go to dashboard</span>
+          </ModalButton>
+        ) : (
+          <ModalButton isLoading={loading} onClick={() => refetch()}>
+            <span>Verify site code</span>
+          </ModalButton>
+        )}
       </ModalFooter>
     </Modal>
   );
